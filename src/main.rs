fbefa24089b0749aa19c51b35cdf02c83af8c21f
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-12-07 22:04:03 +0200

    move/zoom image using hjkl/io

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,29 @@
 //! peeky
 
+#![allow(
+	clippy::iter_nth_zero,
+	clippy::let_and_return,
+	clippy::useless_format,
+)]
+
+#![deny(
+	unconditional_recursion,
+	unknown_lints,
+	unreachable_code,
+	unreachable_patterns,
+	unused_variables,
+	//clippy::as_conversions,
+	//clippy::cast_possible_truncation,
+	//clippy::cast_possible_wrap,
+	//clippy::cast_precision_loss,
+	//clippy::cast_sign_loss,
+	clippy::fn_to_numeric_cast_any,
+	//clippy::format_empty_string, // doesnt exist :c
+	clippy::only_used_in_recursion,
+	clippy::println_empty_string,
+	clippy::unnecessary_cast,
+)]
+
 use clap::Parser;
 use image::{ImageReader, Rgb};
 use minifb::{Key, Window, WindowOptions};
@@ -65,6 +89,15 @@ fn main() {
 	let img = ImageReader::open(&filepath).unwrap().decode().unwrap();
 	let pixels = img.into_rgb8();
 
+	let zoom_default = 0.001; // TODO: calc from image and window sizes
+	let cam_x_default: float = pixels.dimensions().0 as float / 2.;
+	let cam_y_default: float = pixels.dimensions().1 as float / 2.;
+	let mut zoom : float = zoom_default;
+	let mut cam_x: float = cam_x_default; // TODO: make it centered by default
+	let mut cam_y: float = cam_y_default; // TODO: make it centered by default
+	let cam_move_step: float = 0.1;
+	let cam_zoom_step: float = 1.1;
+
 	let mut frame_i: u64 = 0;
 	#[allow(unused_labels)]
 	'mainloop: while window.is_open() && !window.is_key_down(Key::Escape) {
@@ -82,30 +115,81 @@ fn main() {
 			is_redraw_needed = true;
 		}
 
-		if window.is_key_pressed_once(Key::H) {
-			// TODO
+		let cam_move_step = cam_move_step / zoom;
+		if window.is_key_pressed_repeat(Key::H) {
+			cam_x -= cam_move_step;
+			is_redraw_needed = true;
 		}
-		if window.is_key_pressed_once(Key::J) {
-			// TODO
+		if window.is_key_pressed_repeat(Key::J) {
+			cam_y += cam_move_step;
+			is_redraw_needed = true;
 		}
-		if window.is_key_pressed_once(Key::K) {
-			// TODO
+		if window.is_key_pressed_repeat(Key::K) {
+			cam_y -= cam_move_step;
+			is_redraw_needed = true;
 		}
-		if window.is_key_pressed_once(Key::L) {
-			// TODO
+		if window.is_key_pressed_repeat(Key::L) {
+			cam_x += cam_move_step;
+			is_redraw_needed = true;
+		}
+
+		// Compute center world coords BEFORE changing zoom
+		let scx = (w as float) / 2.; // screen center x
+		let scy = (h as float) / 2.; // screen center y
+		let center_world_before = screen_to_image(STW{x:scx, y:scy, wf: w as float, hf: h as float, cam_x, cam_y, zoom});
+
+		if window.is_key_pressed_repeat(Key::I) {
+			zoom *= cam_zoom_step;
+			is_redraw_needed = true;
+		}
+		if window.is_key_pressed_repeat(Key::O) {
+			zoom /= cam_zoom_step;
+			is_redraw_needed = true;
+		}
+
+		let mut is_zoom_reset: bool = false;
+		if window.is_key_pressed_repeat(Key::R) {
+			is_zoom_reset = true;
+			zoom  = zoom_default;
+			cam_x = cam_x_default;
+			cam_y = cam_y_default;
+			if verbose { eprintln!("zoom & position reset") }
+			is_redraw_needed = true;
+		}
+		if window.is_key_pressed_repeat(Key::Z) {
+			is_zoom_reset = true;
+			zoom  = zoom_default;
+			if verbose { eprintln!("zoom reset") }
+			is_redraw_needed = true;
 		}
 
 		if is_redraw_needed {
 			frame_i += 1;
 			if verbose { eprintln!("\nframe {frame_i}:") }
 
+			if verbose { println!("cam xyz: {cam_x}, {cam_y}, zoom={zoom}") }
+			if !is_zoom_reset {
+				// Compute center world coords AFTER zoom
+				let center_world_after = screen_to_image(STW{x:scx, y:scy, wf: w as float, hf: h as float, cam_x, cam_y, zoom});
+				// Adjust camera so center remains fixed
+				cam_x += center_world_before.0 - center_world_after.0;
+				cam_y += center_world_before.1 - center_world_after.1;
+			}
+
 			buffer.fill(BG_COLOR.0);
 
-			for y in 0..pixels.height() {
-				for x in 0..pixels.width() {
-					if let Some(buffer_pixel) = buffer.get_mut(xy_to_buf_index(x, y, w)) {
-						let rgb = pixels.get_pixel(x, y);
-						*buffer_pixel = Color::from(*rgb).0;
+			let (image_w, image_h) = pixels.dimensions();
+			for buf_y in 0 .. h as u32 {
+				for buf_x in 0 .. w as u32 {
+					let (img_x, img_y) = screen_to_image(STW{x: buf_x as float, y: buf_y as float, wf: w as float, hf: h as float, cam_x, cam_y, zoom});
+					let img_x: i32 = img_x as _;
+					let img_y: i32 = img_y as _;
+					if !(0 <= img_x && img_x < image_w as i32) { continue }
+					if !(0 <= img_y && img_y < image_h as i32) { continue }
+					let img_x: u32 = img_x as _;
+					let img_y: u32 = img_y as _;
+					if let Some(img_pixel) = pixels.get_pixel_checked(img_x, img_y) {
+						buffer[xy_to_buf_index(buf_x, buf_y, w)] = Color::from(*img_pixel).0;
 					}
 				}
 			}
@@ -119,6 +203,26 @@ const UNABLE_TO_UPDATE_WINDOW_BUFFER: &str = "unable to update window buffer";
 
 fn xy_to_buf_index(x: u32, y: u32, w: usize) -> usize {
 	(x as usize) + w * (y as usize)
+}
+
+
+
+struct STW {
+	x: float,
+	y: float,
+	/// screen_w
+	wf: float,
+	/// screen_h
+	hf: float,
+	cam_x: float,
+	cam_y: float,
+	zoom: float,
+}
+fn screen_to_image(STW { x, y, wf, hf, cam_x, cam_y, zoom }: STW) -> (float, float) {
+	let aspect_ratio = hf / wf;
+	let world_x = cam_x + (x - wf * 0.5) / (wf * 0.5) / zoom;
+	let world_y = cam_y + (y - hf * 0.5) / (hf * 0.5) / zoom * aspect_ratio;
+	(world_x, world_y)
 }
 
 
@@ -151,6 +255,9 @@ fn rgb_to_u32([r,g,b]: [u8; 3]) -> u32 {
 }
 
 
+
+#[allow(non_camel_case_types)]
+type float = f32;
 
 
 
