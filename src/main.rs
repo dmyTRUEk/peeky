34a29f4a6272066a3d5c9f52dcab36a22cc74695
dmyTRUEk <25669613+dmyTRUEk@users.noreply.github.com>
2025-12-07 17:25:04 +0200

    fix window resizing

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,19 @@ use minifb::{Key, Window, WindowOptions};
 	",
 )]
 struct CliArgs {
+	// verbose output
+	#[arg(short='v', long, default_value_t=false)]
+	verbose: bool,
 }
 
 
 
 fn main() {
 	let CliArgs {
+		verbose,
 	} = CliArgs::parse();
+
+	// TODO: config file
 
 	let (mut w, mut h) = (1600, 900);
 	let mut buffer: Vec<u32> = vec![BG_COLOR.0; w * h];
@@ -46,16 +52,20 @@ fn main() {
 	window.set_target_fps(60);
 	window.update_with_buffer(&buffer, w, h).expect(UNABLE_TO_UPDATE_WINDOW_BUFFER);
 
+	let mut frame_i: u64 = 0;
 	#[allow(unused_labels)]
 	'mainloop: while window.is_open() && !window.is_key_down(Key::Escape) {
-		let mut is_redraw_needed: bool = false;
+		let mut is_redraw_needed: bool = frame_i == 0; // condition needed to render first frame
 
 		// handle resizing
+		let wh_prev = (w, h);
 		(w, h) = window.get_size();
-		let new_size = w * h;
-		if new_size != buffer.len() {
-			buffer.resize(new_size, BG_COLOR.0);
-			//if verbose { println!("Resized to {w}x{h}") }
+		if (w, h) != wh_prev {
+			#[allow(irrefutable_let_patterns)]
+			if let new_size = w * h && new_size != buffer.len() {
+				buffer.resize(new_size, BG_COLOR.0);
+			}
+			if verbose { eprintln!("Resized to {w}x{h}") }
 			is_redraw_needed = true;
 		}
 
@@ -73,12 +83,17 @@ fn main() {
 		}
 
 		if is_redraw_needed {
+			frame_i += 1;
+			if verbose { eprintln!("\nframe {frame_i}:") }
+
+			buffer.fill(BG_COLOR.0);
+
 			// TODO
 		} // end of render
 
 		window.update_with_buffer(&buffer, w, h).expect(UNABLE_TO_UPDATE_WINDOW_BUFFER);
 	} // end of main loop
-}
+} // end of main
 
 const UNABLE_TO_UPDATE_WINDOW_BUFFER: &str = "unable to update window buffer";
 
